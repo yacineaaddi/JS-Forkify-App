2bca94045a43145db645ac9bae60b384be1aec58
yacine <yacineaaddi@gmail.com>
2025-07-03 18:11:13 +0100

    Listening For load and hashchange Events

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,5 @@
-import icons from '../img/icons.svg';
-import 'core-js/table';
+import icons from 'url:../img/icons.svg';
+import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
@@ -27,10 +27,13 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    if (!id) return;
     // 1 - Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message}(${res.status})`);
@@ -152,4 +155,5 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
